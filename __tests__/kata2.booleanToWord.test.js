81eac2594f54ae1cc501b541224de34276976788
Cat Hallam <catherinehallam@mutumojaa.com>
2018-06-16 17:45:10 +0100

    Message

--- __tests__/kata2.booleanToWord.test.js
+++ __tests__/kata2.booleanToWord.test.js
@@ -1,5 +1,14 @@
 const { booleanToWord } = require('../src');
 
-describe('booleanToWord', () => {
+describe('Determine if true or false', () => {
   // how do we create specs again???
+  it('returns Yes if passed true', () => {
+    expect(booleanToWord(true)).toBe("Yes");
+  });
+
+  it('returns No if passed false', () => {
+    expect(booleanToWord(false)).toBe("No");
+  });
 });
+
+
--- __tests__/kata4.humanCatDogYears.test.js
+++ __tests__/kata4.humanCatDogYears.test.js
@@ -1,3 +1,10 @@
 const { humanCatDogYears } = require('../src');
 
-// Look Ma, no handlebars!!!
+
+describe('When humanCatDogYears is passed a number (representing human years)', () => {
+  it('should return an array of three numbers', () => {
+    expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+  });
+});
+
+
