77c49d279b4a88c7d034fe28d5ad638f73027f5b
Cat Hallam <catherinehallam@mutumojaa.com>
2018-06-16 19:41:28 +0100

    TDD with Jest

--- __tests__/kata6.joinNames.test.js
+++ __tests__/kata6.joinNames.test.js
@@ -2,6 +2,19 @@ const { joinNames } = require('../src');
 
 describe('joinNames', () => {
   it('returns string of names, seperated by commas and an ampersand', () => {
-    
+    const array = [{
+      name: "Bart"
+    }, {
+      name: "Lisa"
+    }, {
+      name: "Maggie"
+    }];
+
+    expect(joinNames(array)).toBe("Bart, Lisa, Maggie");
+
   });
 });
+
+
+
+
--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,5 +1,38 @@
 const joinNames = (namesObj) => {
 
+  //STEP OPTION 1
+  //use a map to convert array of objects into arrays of strings
+  //join the array with comma space
+  //
+//STEPS OPTION 1
+  //use map to create array of names
+  //use pop to take the last name off the name of the array and store in a variable
+  //use join with comma and space to format the first two names. Use concat to add maggie
+  //template string
+  [
+    {name: "Bart"},
+    {name: "Lisa"},
+    {name: "Maggie"}
+  ].map(function(array){
+      return array.name;
+  }).join(",");
 }
+
+  //array.map(x => x.name).join(', ');
+  //const array = [{
+   // name: 'Bart'
+  //}, {
+  //  name: 'Lisa'
+  //}, {
+  //  name: 'Maggie'
+  //}];
+
+  //for(let i = 0; i < array.length; i++){
+  //console.log(array[i].name);
+
+
+//}
+
+
 
 module.exports = joinNames;
